c76b4faf948bd12e73a4a6210d5c4fef983c8788
Natasha Wangui <69792432+NatashaJWangui@users.noreply.github.com>
2025-06-19 13:39:41 +0300

    Modified the method call in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
   has_many :comments
 
   validates :title, presence: true, length: { in: 5..300 }
-  validates :url, presence: true, format: { with: URI::regexp(%w[http https]) }
+  validates :url, presence: true, format: { with: URI.regexp(%w[http https]) }
   validates :user_id, presence: true
 end
