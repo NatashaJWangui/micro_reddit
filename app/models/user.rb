ce129bcd3acbc1fc2f662bb23d25f2576860cb61
Natasha Wangui <69792432+NatashaJWangui@users.noreply.github.com>
2025-06-19 13:12:08 +0300

    Add User validations and tested in console

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { in: 3..20 }
+
+  validates :email, presence: true,
+                   uniqueness: { case_sensitive: false },
+                   format: { with: URI::MailTo::EMAIL_REGEXP }
 end
