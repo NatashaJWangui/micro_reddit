2a34ca0331949b16cdd640544a2c9f48ee23cd44
Natasha Wangui <69792432+NatashaJWangui@users.noreply.github.com>
2025-06-19 13:24:41 +0300

    Add Comment model with full associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
   validates :title, presence: true, length: { in: 5..300 }
   validates :url, presence: true, format: { with: URI::regexp(%w[http https]) }
