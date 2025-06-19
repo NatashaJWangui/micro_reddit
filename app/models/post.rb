918e2642344bd10847a437183332875174f20fe8
Natasha Wangui <69792432+NatashaJWangui@users.noreply.github.com>
2025-06-19 13:22:22 +0300

    Add Post model with validations and association belongs_to

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: { in: 5..300 }
+  validates :url, presence: true, format: { with: URI::regexp(%w[http https]) }
+  validates :user_id, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_19_100504) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_19_101231) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "users"
 end
